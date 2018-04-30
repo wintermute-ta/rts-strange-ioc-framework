cb703071a37069b1939796eba59db88ad82c63ce
Ivan Legostaev <ilegostaev.dev@gmail.com>
2018-04-30 07:23:33 +0000

    - Guns shooting. - Guns destroying ships - Guns have attack range and check out the enemies in it range

---
+++ project/Assets/Scripts/Scripts/HexMapChunk.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexMapChunk
+{
+    public int X;
+    public int Z;
+    public event Action OnChanged = delegate { };
+
+    HexMapCell[] cells;
+
+    public HexMapChunk(int x, int z, int chunkSizeX, int chunkSizeZ)
+    {
+        cells = new HexMapCell[chunkSizeX * chunkSizeZ];
+        X = x;
+        Z = z;
+    }
+
+    public void AddCell(int index, HexMapCell cell)
+    {
+        cells[index] = cell;
+        cell.Chunk = this;
+    }
+
+    public Vector2 GetRoadInterpolators(HexDirection direction, HexMapCell cell)
+    {
+        Vector2 interpolators;
+        if (cell.HasRoadThroughEdge(direction))
+        {
+            interpolators.x = interpolators.y = 0.5f;
+        }
+        else {
+            interpolators.x =
+                cell.HasRoadThroughEdge(direction.Previous()) ? 0.5f : 0.25f;
+            interpolators.y =
+                cell.HasRoadThroughEdge(direction.Next()) ? 0.5f : 0.25f;
+        }
+        return interpolators;
+    }
+
+    public void Refresh()
+    {
+        OnChanged.Invoke();
+    }
+
+}
---
+++ project/Assets/Scripts/Scripts/Units/InstanceGun.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+/// <summary>
+/// Description of the instance gun
+/// </summary>
+public class InstanceGun : IUnit
+{
+    public int ID { get; set; }
+    public HexCoordinates Coordinates { get; set; }
+    public int AttackRange { get; set; }
+    public float HealthPoint { get; set; }
+    public float DamagePerSecond { get; set; }
+    public List<HexCoordinates> AttackCoordinates { get; set; }
+
+    public InstanceGun(HexCoordinates coordinates)
+    {
+        Coordinates = coordinates;
+        AttackRange = 3;
+        HealthPoint = 10;
+        AttackCoordinates = new List<HexCoordinates>();
+
+        #region AttackCoordinates update. For gun list AttackCoordinates update one time.
+        var hexMapCell = GameManager.Instance.Map.GetCell(coordinates);
+        var neighbors = new List<HexCoordinates>();
+        var iteration = AttackRange;
+        while (iteration != 0)
+        {
+            if (!neighbors.Any())
+            {
+                //1st iteration. 
+                foreach (var cell in hexMapCell.Neighbors)
+                {
+                    AttackCoordinates.Add(cell.Coordinates);
+                    neighbors.Add(cell.Coordinates);
+                }
+            }
+            else
+            {
+                //more then 1st iteration.
+                var neighbors2 = new List<HexCoordinates>();
+                foreach (var cell in neighbors)
+                {
+                    hexMapCell = GameManager.Instance.Map.GetCell(cell);
+                    foreach (var c in hexMapCell.Neighbors)
+                    {
+                        neighbors2.Add(c.Coordinates);
+                    }
+                }  
+                //get distinct elements
+                neighbors2 = neighbors2.Distinct().ToList();
+                AttackCoordinates.AddRange(neighbors2);
+                neighbors = neighbors2;
+            }
+            iteration--;
+           
+        }
+        //get distinct elements
+        AttackCoordinates = AttackCoordinates.Distinct().ToList();
+        #endregion
+    }
+
+}
